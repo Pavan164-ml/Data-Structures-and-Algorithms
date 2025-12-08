6a6fe7bd2734f59e4347568363e0c2373716b415
Pavan Kumar <59686265+Pavan164-ml@users.noreply.github.com>
2025-12-08 10:17:39 +0530

    Update TwoSum.py

--- Two Pointers Approach/TwoSum.py
+++ Two Pointers Approach/TwoSum.py
@@ -12,6 +12,20 @@ class Solution:
 # 3. we just need to find out the next number which could be at jth index
 # 4. This is by taversing the entire array at 2 scale 
 # 5. At each step checking if the sum of ith and jth index elements match the target or not
-
 # Correction I had missed the edge case where i th index and jth index shouldnt be same as it would make no sense. So after that submission was perfect
 # Need to optimize the time complixity from O(N2)
+
+
+# Hashmap Approach
+# SC : O(N)
+# TC : O(N)
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {} # Map that holds #value and index
+
+        for i,num in enumerate(nums): # iterates through each index and the actual value of the list 
+            check = target - num # checks if the difference between target and current number is present in the list. If yes then we will return.
+            if check in seen:
+                return [seen[check],i] # this will only return the index not the number, because hashmap key is number and value is index here 
+            seen[num]= i  # if the difference is not present we add the key as number and value as current index for future iterations
+        return None # if the loop is completed and we dont find any answes then there is no result
